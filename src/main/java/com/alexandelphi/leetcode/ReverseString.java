70573bb3f22ac762b8fe61364a7b56e0ce4b9334
Oleksandr Kononenko <o.kononenko@spd-ukraine.com>
2018-09-02 20:20:27 +0300

    addd ReverseString and gitignore

---
+++ src/main/java/com/alexandelphi/leetcode/ReverseString.java
@@ -0,0 +1,27 @@
+package com.alexandelphi.leetcode;
+
+class ReverseString {
+    public String reverseString(String s) {
+        char[] temp = new char[s.length()];
+        int i = 0;
+        int j = s.length()-1;
+        
+        while (i <= j) {
+            temp[i] = s.charAt(j);
+            temp[j] = s.charAt(i);
+            i++;
+            j--;
+        }
+        return new String(temp);
+    }
+}
+
+
+class Main {
+    public static void main(String[] args) {
+        ReverseString reverse = new ReverseString();
+
+        String input = "hello";
+        System.out.println(reverse.reverseString(input));
+    }
+}
