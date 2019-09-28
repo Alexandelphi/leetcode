5a9c75c829a84fd39828c8d1912b1ecbe58701a8
Oleksandr Kononenko <o.kononenko@spd-ukraine.com>
2019-09-29 01:13:58 +0300

    Added "1160. Find Words That Can Be Formed by Characters"

---
+++ src/main/java/com/alexandelphi/leetcode/easy/FindWordsThatCanBeFormedByCharacters.java
@@ -0,0 +1,26 @@
+package com.alexandelphi.leetcode.easy;
+
+public class FindWordsThatCanBeFormedByCharacters {
+  public int countCharacters(String[] words, String chars) {
+    int[] charTable = new int[26];
+    for (char c : chars.toCharArray()) {
+      charTable[c - 'a']++;
+    }
+    int sum = 0;
+    for (String word : words) {
+      int i = 0;
+      int[] charTableCopy = charTable.clone();
+      for (char c : word.toCharArray()) {
+        if (charTableCopy[c - 'a']-- > 0) {
+          i++;
+        } else {
+          break;
+        }
+      }
+      if (i == word.length()) {
+        sum += i;
+      }
+    }
+    return sum;
+  }
+}
