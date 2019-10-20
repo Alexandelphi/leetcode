cefdcf6f21a96c2a3391ef7661e95c0e2db4ea82
Oleksandr Kononenko <o.kononenko@spd-ukraine.com>
2019-10-20 22:54:02 +0300

    Updated "215. Kth Largest Element in an Array", added optimized solution

--- src/main/java/com/alexandelphi/leetcode/medium/KthLargestElementInAnArray.java
+++ src/main/java/com/alexandelphi/leetcode/medium/KthLargestElementInAnArray.java
@@ -6,10 +6,9 @@ import java.util.PriorityQueue;
 
 public class KthLargestElementInAnArray {
 
-  //  Runtime: 2 ms, faster than 88.83% of Java online submissions for Kth Largest Element in an Array.
-  //  Memory Usage: 38.1 MB, less than 51.30% of Java online submissions for Kth Largest Element in an Array.
-
-  //  my solution with using sorting
+  // Runtime: 2 ms, faster than 88.83% of Java online submissions for Kth Largest Element in an Array.
+  // Memory Usage: 38.1 MB, less than 51.30% of Java online submissions for Kth Largest Element in an Array.
+  // my 2nd solution with using sorting
   public int findKthLargestV2(int[] nums, int k) {
     if (k < 0) {
       return -1;
@@ -18,7 +17,7 @@ public class KthLargestElementInAnArray {
     return nums[nums.length - k];
   }
 
-  // my solution with using HeapSort (PriorityQueue)
+  // my 1st solution with using HeapSort (PriorityQueue), straightforward approach ...
   public int findKthLargestV1(int[] nums, int k) {
     PriorityQueue<Integer> heap = new PriorityQueue<>(Collections.reverseOrder());
     for (int i : nums) {
@@ -29,5 +28,19 @@ public class KthLargestElementInAnArray {
       result = heap.remove();
     }
     return result;
+  }
+
+  // Runtime: 6 ms, faster than 62.22% of Java online submissions for Kth Largest Element in an Array.
+  // Memory Usage: 36.7 MB, less than 90.67% of Java online submissions for Kth Largest Element in an Array.
+  // my 3rd solution with using PriorityQueue, optimized version
+  public int findKthLargestV3(int[] nums, int k) {
+    PriorityQueue<Integer> heap = new PriorityQueue<>();
+    for (int i : nums) {
+      heap.add(i);
+      if (heap.size() > k) {
+        heap.remove();
+      }
+    }
+    return heap.remove();
   }
 }
