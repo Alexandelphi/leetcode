f6c3a37b157ac88af2bb3dcc1e862a7873fd4483
Oleksandr Kononenko <o.kononenko@spd-ukraine.com>
2019-12-04 01:39:28 +0200

    Updated "245. Shortest Word Distance III", HashMap solution

--- src/main/java/com/alexandelphi/leetcode/medium/ShortestWordDistanceIII.java
+++ src/main/java/com/alexandelphi/leetcode/medium/ShortestWordDistanceIII.java
@@ -11,7 +11,7 @@ public class ShortestWordDistanceIII {
   // Runtime: 11 ms, faster than 5.80% of Java online submissions for Shortest Word Distance III.
   // Memory Usage: 38.7 MB, less than 10.00% of Java online submissions for Shortest Word Distance III.
   // my 1st solution
-  public int shortestWordDistance(String[] words, String word1, String word2) {
+  public int shortestWordDistance1(String[] words, String word1, String word2) {
     Map<String, List<Integer>> wordIndicesMap = new HashMap<>();
     for (int i = 0; i < words.length; i++) {
       String word = words[i];
@@ -40,5 +40,34 @@ public class ShortestWordDistanceIII {
       }
     }
     return minDistance;
+  }
+
+  // Runtime: 2 ms, faster than 54.29% of Java online submissions for Shortest Word Distance III.
+  // Memory Usage: 36.6 MB, less than 100.00% of Java online submissions for Shortest Word Distance III.
+  // my 2nd solution
+  public int shortestWordDistance2(String[] words, String word1, String word2) {
+    int index1 = -1;
+    int index2 = -1;
+    int result = Integer.MAX_VALUE;
+    boolean shouldChange = true;
+    for (int i = 0; i < words.length; i++) {
+      if (!word1.equals(word2)) {
+        if (words[i].equals(word1)) index1 = i;
+        if (words[i].equals(word2)) index2 = i;
+      } else {
+        if (words[i].equals(word1) && shouldChange) {
+          index1 = i;
+          shouldChange = false;
+        } else if (words[i].equals(word2)) {
+          index2 = i;
+          shouldChange = true;
+        }
+      }
+
+      if (index1 != -1 && index2 != -1) {
+        result = Math.min(result, Math.abs(index2 - index1));
+      }
+    }
+    return result;
   }
 }
