53e3bdf8a150178970049237eed590cef43d812f
Oleksandr Kononenko <o.kononenko@spd-ukraine.com>
2020-01-04 01:46:02 +0200

    Added "322. Coin Change", recursive and iterative dp solution

---
+++ src/main/java/com/alexandelphi/leetcode/medium/CoinChange.java
@@ -0,0 +1,53 @@
+package com.alexandelphi.leetcode.medium;
+
+import java.util.Arrays;
+
+public class CoinChange {
+
+  // Runtime: 14 ms, faster than 48.83% of Java online submissions for Coin Change.
+  // Memory Usage: 35.9 MB, less than 95.27% of Java online submissions for Coin Change.
+  public int coinChange(int[] coins, int amount) {
+    int[] dp = new int[amount + 1];
+    Arrays.fill(dp, amount + 1);
+    dp[0] = 0;
+    for (int i = 1; i < dp.length; i++) {
+      for (int coin : coins) {
+        if (i - coin < 0) {
+          continue;
+        }
+        dp[i] = Math.min(dp[i - coin] + 1, dp[i]);
+      }
+    }
+    return dp[amount] > amount ? -1 : dp[amount];
+  }
+
+
+
+  //  Runtime: 27 ms, faster than 16.35% of Java online submissions for Coin Change.
+  //  Memory Usage: 35.7 MB, less than 98.82% of Java online submissions for Coin Change.
+  public int coinChangeRecursion(int[] coins, int amount) {
+    if (amount < 0) {
+      return 0;
+    }
+    return coinHandler(coins, amount, new int[amount]);
+  }
+
+  private int coinHandler(int[] coins, int rem, int[] memo) {
+    if (rem < 0) return -1;
+    if (rem == 0) return 0;
+    if (memo[rem - 1] != 0) return memo[rem - 1];
+    int min = rem + 1;
+    for (int coin : coins) {
+      int result = coinHandler(coins, rem - coin, memo);
+      if (result >= 0 && result < min) {
+        min = result + 1;
+      }
+    }
+    if (min == rem + 1) {
+      memo[rem - 1] = -1;
+    } else {
+      memo[rem - 1] = min;
+    }
+    return memo[rem - 1];
+  }
+}
