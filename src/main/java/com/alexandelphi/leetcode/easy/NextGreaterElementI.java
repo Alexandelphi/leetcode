34ce8d6f3a04285812a1733170ef945991182604
Oleksandr Kononenko <o.kononenko@spd-ukraine.com>
2020-01-08 00:27:04 +0200

    Added "496. Next Greater Element I", new solution based on Stack + HashMap

--- src/main/java/com/alexandelphi/leetcode/easy/NextGreaterElementI.java
+++ src/main/java/com/alexandelphi/leetcode/easy/NextGreaterElementI.java
@@ -1,6 +1,10 @@
 package com.alexandelphi.leetcode.easy;
 
+import java.util.ArrayDeque;
 import java.util.Arrays;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.Map;
 
 public class NextGreaterElementI {
 
@@ -24,4 +28,23 @@ public class NextGreaterElementI {
      }
      return result;
    }
+
+  // Runtime: 3 ms, faster than 80.92% of Java online submissions for Next Greater Element I.
+  // Memory Usage: 37 MB, less than 100.00% of Java online submissions for Next Greater Element I.
+  public int[] nextGreaterElementV2(int[] nums1, int[] nums2) {
+    int n = nums1.length;
+    int[] result = new int[n];
+    Deque<Integer> stack = new ArrayDeque<>();
+    Map<Integer, Integer> map = new HashMap<>();
+    for (int num : nums2) {
+      while (!stack.isEmpty() && num > stack.peek()) {
+        map.put(stack.pop(), num);
+      }
+      stack.push(num);
+    }
+    for (int i = 0; i < n; i++) {
+      result[i] = map.getOrDefault(nums1[i], -1);
+    }
+    return result;
+  }
 }
