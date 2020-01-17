25b8097905f76bc243b67fb3606046f7ab9304b5
Oleksandr Kononenko <o.kononenko@spd-ukraine.com>
2020-01-18 00:05:49 +0200

    Updated "266. Palindrome Permutation", added an alternative solution with HashSet

--- src/main/java/com/alexandelphi/leetcode/easy/PalindromePermutation.java
+++ src/main/java/com/alexandelphi/leetcode/easy/PalindromePermutation.java
@@ -1,5 +1,8 @@
 package com.alexandelphi.leetcode.easy;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class PalindromePermutation {
 
   // Runtime: 0 ms, faster than 100.00% of Java online submissions for Palindrome Permutation.
@@ -15,5 +18,18 @@ public class PalindromePermutation {
       result += (count[i] % 2);
     }
     return result < 2;
+  }
+
+  // Runtime: 1 ms, faster than 66.93% of Java online submissions for Palindrome Permutation.
+  // Memory Usage: 41.1 MB, less than 5.88% of Java online submissions for Palindrome Permutation.
+  // alternative version with HashSet
+  public boolean canPermutePalindromeV2(String s) {
+    Set<Character> set = new HashSet<>();
+    for (int i = 0; i < s.length(); i++) {
+      if (!set.add(s.charAt(i))) {
+        set.remove(s.charAt(i));
+      }
+    }
+    return set.size() < 2;
   }
 }
