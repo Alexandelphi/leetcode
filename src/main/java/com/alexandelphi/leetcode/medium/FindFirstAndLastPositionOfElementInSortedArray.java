d4340233210177e16a59f0e87a19535081e31be7
Oleksandr Kononenko <o.kononenko@spd-ukraine.com>
2020-02-20 00:53:37 +0200

    Updated "34. Find First and Last Position of Element in Sorted Array", BinarySearch solution

--- src/main/java/com/alexandelphi/leetcode/medium/FindFirstAndLastPositionOfElementInSortedArray.java
+++ src/main/java/com/alexandelphi/leetcode/medium/FindFirstAndLastPositionOfElementInSortedArray.java
@@ -41,3 +41,41 @@ public class FindFirstAndLastPositionOfElementInSortedArray {
     return low;
   }
 }
+
+
+// Runtime: 0 ms, faster than 100.00% of Java online submissions for Find First and Last Position of Element in Sorted Array.
+// Memory Usage: 42.6 MB, less than 100.00% of Java online submissions for Find First and Last Position of Element in Sorted Array.
+// another solution, task review and re-practice
+class Solution {
+  public int[] searchRange(int[] nums, int target) {
+    return new int[] {
+        searchFirstOrLast(nums, target, true),
+        searchFirstOrLast(nums, target, false)
+    };
+  }
+
+  private int searchFirstOrLast(int[] nums, int target, boolean findFirst) {
+    if (nums == null || nums.length == 0) {
+      return -1;
+    }
+    if (nums.length == 1) {
+      return nums[0] == target ? 0 : -1;
+    }
+    int low = 0;
+    int high = nums.length;
+    if (!findFirst) ++target;
+    while (low < high) {
+      int mid = low + (high - low) / 2;
+      if (nums[mid] < target) {
+        low = mid + 1;
+      } else {
+        high = mid;
+      }
+    }
+    if (findFirst) {
+      return (low < nums.length && nums[low] == target) ? low : -1;
+    } else {
+      return (low > 0 && nums[low - 1] == --target) ? low - 1 : -1;
+    }
+  }
+}
